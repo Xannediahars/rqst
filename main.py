2bd8e9f7af66e8a9b0b63ad8f2ab8e255cbc42c7
Vivienn <104738150+Xannediahars@users.noreply.github.com>
2024-01-08 02:14:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+# Request to REST API
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+
+# Print response in JSON format
+print(response.json())
+import requests
+
+# Request to REST API
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+
+# Print response in JSON format
+print(response.json())
